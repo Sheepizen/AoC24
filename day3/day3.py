599ea204b5167ec902ed217f1c83e26651e2201a
Jonatan <Jonatan.goldbach@gmx.de>
2024-12-08 01:03:56 +0100

    day3 second star

--- day3/day3.py
+++ day3/day3.py
@@ -5,14 +5,29 @@ def use_regex(input_text):
     pattern = re.compile(r"mul\([0-9]+,[0-9]+\)", re.IGNORECASE)
     return pattern.findall(input_text)
 
+def find_do_dont(input_text):
+     pattern = re.compile(r"don\'t\(\).*?do\(\)?",re.IGNORECASE)
+     return pattern.findall(input_text)
+
+def remove_do_dont(input_text):
+    removedString = input_text
+    for removeableString in find_do_dont(input_text):
+        removedString = removedString.replace(removeableString,' ')
+    return removedString
+
 def parseData(txt):
-    mulArr =[]
-    numsArr =[]
     f = open(txt, "r")
     lines = str(f.readlines())
-    regexedArr = use_regex(lines)
+    ## need to add do so regex finds the the last dont block to the end
+    lines = lines + "do()"
+    return lines.strip()
+
+def getNums(txt):
+    numsArr = []
+    regexedArr = use_regex(txt)
     for match in regexedArr:
         numsArr.append(match.split("l")[-1].strip("(").strip(")").split(","))
+        print(  match.split("l")[-1].strip("(").strip(")").split(","))
     return numsArr
 
 def calculateData(numsArr):
@@ -22,4 +37,6 @@ def calculateData(numsArr):
     return sum
 
 ### FIRST STAR
-print(calculateData(parseData("input.txt")))
+#print(calculateData(getNums(parseData("input.txt"))))
+## SECOND STAR
+print(calculateData(getNums(remove_do_dont(parseData("input.txt")))))
